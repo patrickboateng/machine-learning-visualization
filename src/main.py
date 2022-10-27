afc829075c83f1ffbc223e7515c58b621c7c0c45
Pato <boatengpato.pb@gmail.com>
2022-10-27 19:53:19 +0000

    minor change

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from algorithms.cost_functions import (
     mean_squared_error,
@@ -10,13 +12,16 @@ from algorithms.gradient_descent import batch_gradient_descent, hypothesis
 
 
 if __name__ == "__main__":
-    df = np.loadtxt("../test_data/ex1data1.txt", delimiter=",")
+    # df = np.loadtxt("../test_data/ex1data1.txt", delimiter=",")
 
-    X = df[:, 0]
-    y = df[:, 1]
+    # X = df[:, 0]
+    # y = df[:, 1]
 
-    # X = np.array([20, 25, 27, 29, 35, 38, 40])
-    # y = np.array([83, 90, 93, 95, 99, 110, 120])
+    # X = np.array([1, 2, 3, 4])
+    # y = np.array([1.5, 1.6, 2.1, 3.0])
+
+    X = np.array([20, 25, 27, 29, 35, 38, 40])
+    y = np.array([83, 90, 93, 95, 99, 110, 120])
 
     # X = X.reshape(-1, 1)
 
@@ -24,13 +29,18 @@ if __name__ == "__main__":
 
     # coefficient_of_constant = np.ones((m, 1))
 
-    # X_mod = np.hstack((coefficient_of_constant, X))
+    # X = np.hstack((coefficient_of_constant, X))
 
-    # _, n = X_mod.shape
+    # _, n = X.shape
 
     # theta = np.zeros((n, 1))
 
-    print(batch_gradient_descent(X, y, cost_function="mse"))
+    theta, _ = batch_gradient_descent(X, y, cost_function="mse")
+    print(theta)
+
+    # plt.plot(bgdinfo.num_of_epochs, bgdinfo.cost_history)
+
+    # print(mean_squared_error(X, y, theta))
 
     # for x in X:
     #     print(hypothesis(np.array([[1, x[0]]]), np.array([[0.0322], [0.0876]])))
