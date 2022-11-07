693658eb802ef270227a7f46189d8b2255b5d91b
Pato <boatengpato.pb@gmail.com>
2022-11-07 00:42:37 +0000

    minor change

--- src/algorithms/__init__.py
+++
@@ -1,1 +0,0 @@
-from algorithms.errors import InconsistentLengthException
--- src/main.py
+++ src/main.py
@@ -1,13 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
-from algorithms.cost_functions import (
-    mean_squared_error,
-    mean_absolute_error,
-)
-
-from algorithms.gradient_descent import BatchGradientDescent
+from optimization_algorithms.optimizers import batch_gradient_descent
 
 
 if __name__ == "__main__":
@@ -19,32 +12,24 @@ if __name__ == "__main__":
     # X = np.array([1, 2, 3, 4])
     # y = np.array([1.5, 1.6, 2.1, 3.0])
 
-    X = np.array([20, 25, 27, 29, 35, 38, 40])
-    y = np.array([83, 90, 93, 95, 99, 110, 120])
+    X = np.array([1, 2, 3, 4, 5])
+    y = np.array([4.2, 3.5, 3.0, 3.4, 2.0])
 
-    # X = X.reshape(-1, 1)
+    # X = np.array([20, 25, 27, 29, 35, 38, 40])
+    # y = np.array([83, 90, 93, 95, 99, 110, 120])
 
-    # m = len(X)
+    X = X.reshape(-1, 1)
 
-    # coefficient_of_constant = np.ones((m, 1))
+    m = len(X)
 
-    # X = np.hstack((coefficient_of_constant, X))
+    coefficient_of_constant = np.ones((m, 1))
 
-    # _, n = X.shape
+    X = np.hstack((coefficient_of_constant, X))
 
-    # theta = np.zeros((n, 1))
+    _, n = X.shape
 
-    bgd = BatchGradientDescent(X, y, feature_scaler="min_max")
-    theta, history = bgd.run()
+    theta = np.zeros((n, 1))
     print(theta)
-    i = bgd.transform(np.array([[40]])).flatten()[0]
 
-    print(bgd.hypothesis(np.array([[1, i]]), theta))
-    # plt.plot(bgdinfo.num_of_epochs, bgdinfo.cost_history)
-
-    # print(mean_squared_error(X, y, theta))
-
-    # for x in X:
-    #     print(hypothesis(np.array([[1, x[0]]]), np.array([[0.0322], [0.0876]])))
-
-    # print(hypothesis(np.array([[1, 8.0]]), np.array([[4.57], [-0.45]])))
+    t = batch_gradient_descent(X, y, theta)
+    print(t)
