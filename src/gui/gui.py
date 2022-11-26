562b82cfe83d5c283b0ce8b633f06d42ea7e237e
Pato <boatengpato.pb@gmail.com>
2022-11-26 20:18:35 +0000

    initial app interface completed

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,12 +1,12 @@
-from pathlib import Path
+import pathlib
 
 import wx
 import wx.grid
 from ObjectListView import ObjectListView, ColumnDefn
 
-from . import utils
+from . import controls
 
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = pathlib.Path(__file__).parent.parent
 
 
 class Application(wx.App):
@@ -17,22 +17,6 @@ class Application(wx.App):
         self.SetTopWindow(window)
 
         return True
-
-
-class Feature:
-    __slots__ = "name", "dataType"
-
-    def __init__(self, name, dataType) -> None:
-        self.name = name
-        self.dataType = dataType
-
-
-class Label:
-    __slots__ = "name", "dataType"
-
-    def __init__(self, name, dataType) -> None:
-        self.name = name
-        self.dataType = dataType
 
 
 class MainWindow(wx.Frame):
@@ -62,32 +46,30 @@ class MainPanel(wx.Panel):
         self.init()
 
     def init(self):
-        # self.fileNameDisplay = wx.TextCtrl(self, style=wx.TE_READONLY, size=(200, 35))
-        # self.fileNameDisplay.SetBackgroundColour(colour="#ffffff")
+        self.fileNameDisplay = wx.TextCtrl(self, style=wx.TE_READONLY, size=(200, 35))
+        self.fileNameDisplay.SetBackgroundColour(colour="#ffffff")
 
-        # self.browseBtn = wx.Button(self, label="Browse", size=(60, 35))
-        # self.viewDataBtn = wx.Button(self, label="View Data", size=(60, 35))
+        self.browseBtn = wx.Button(self, label="Browse", size=(60, 35))
 
-        # hSizer = wx.BoxSizer(orient=wx.HORIZONTAL)
-        # hSizer.Add(
-        #     self.fileNameDisplay,
-        #     proportion=1,
-        #     flag=wx.TOP | wx.LEFT,
-        #     border=10,
-        # )
-        # hSizer.Add(self.browseBtn, flag=wx.TOP, border=10)
-        # hSizer.Add(self.viewDataBtn, flag=wx.TOP | wx.RIGHT, border=10)
+        topLayout = wx.BoxSizer(orient=wx.HORIZONTAL)
+        topLayout.Add(
+            self.fileNameDisplay,
+            proportion=1,
+            flag=wx.TOP | wx.LEFT,
+            border=10,
+        )
+        topLayout.Add(self.browseBtn, flag=wx.TOP, border=10)
 
         costFunction = wx.StaticText(self, label="Cost Function, J(\u03B8)")
-        self.costFunctionCtrl = utils.ComboCtrl(self)
+        self.costFunctionCtrl = controls.ComboCtrl(self)
         self.costFunctionCtrl.setUp()
 
         mlAlg = wx.StaticText(self, label="Machine Learning Algorithm")
-        self.mlAlgoCtrl = utils.ComboCtrl(self)
+        self.mlAlgoCtrl = controls.ComboCtrl(self)
         self.mlAlgoCtrl.setUp()
 
         optAlg = wx.StaticText(self, label="Optimization Algorithm")
-        self.optAlgCtrl = utils.ComboCtrl(self)
+        self.optAlgCtrl = controls.ComboCtrl(self)
         self.optAlgCtrl.setUp()
 
         learningRate = wx.StaticText(self, label="Learning Rate, \u03B1")
@@ -98,22 +80,14 @@ class MainPanel(wx.Panel):
             [
                 ColumnDefn(
                     title="Feature Name(s)", valueGetter="name", isSpaceFilling=True
-                ),
-                ColumnDefn(
-                    title="Data Type", valueGetter="dataType", isSpaceFilling=True
-                ),
+                )
             ]
         )
         self.featureDisp.SetEmptyListMsg("No Feature(s) Added")
 
         self.labelDisp = ObjectListView(self, style=wx.LC_REPORT)
         self.labelDisp.SetColumns(
-            [
-                ColumnDefn(title="Label Name", valueGetter="name", isSpaceFilling=True),
-                ColumnDefn(
-                    title="Data Type", valueGetter="dataType", isSpaceFilling=True
-                ),
-            ]
+            [ColumnDefn(title="Label Name", valueGetter="name", isSpaceFilling=True)]
         )
         self.labelDisp.SetEmptyListMsg("No Label Added")
 
@@ -133,29 +107,20 @@ class MainPanel(wx.Panel):
         paramLayout.AddGrowableCol(0, proportion=1)
         paramLayout.AddGrowableCol(1, proportion=1)
 
-        self.dataDisp = utils.DataGrid(self)
+        self.dataDisp = controls.DataGrid(self)
 
-        hSizer = wx.BoxSizer(orient=wx.HORIZONTAL)
-        hSizer.Add(
+        bottomLayout = wx.BoxSizer(orient=wx.HORIZONTAL)
+        bottomLayout.Add(
             paramLayout,
             flag=wx.TOP | wx.RIGHT | wx.LEFT | wx.EXPAND | wx.BOTTOM,
             border=10,
         )
-        hSizer.Add(self.dataDisp, proportion=1, flag=wx.TOP | wx.EXPAND, border=10)
+        bottomLayout.Add(
+            self.dataDisp, proportion=1, flag=wx.TOP | wx.EXPAND, border=10
+        )
 
-        # paramFLSizer.Add(self.dataDisp, proportion=1, flag=wx.EXPAND, border=10)
-        # paramFLSizer.Add(dataDisp, proportion=1, flag=wx.EXPAND, border=10)
+        mainSizer = wx.BoxSizer(orient=wx.VERTICAL)
+        mainSizer.Add(topLayout, flag=wx.EXPAND | wx.RIGHT, border=10)
+        mainSizer.Add(bottomLayout, proportion=1, flag=wx.EXPAND)
 
-        # hSizer.Add(self.featureDisp, proportion=1, flag=wx.RIGHT | wx.EXPAND, border=5)
-        # hSizer.Add(self.labelDisp, proportion=1, flag=wx.EXPAND)
-
-        # h2Sizer = wx.BoxSizer(orient=wx.HORIZONTAL)
-        # h2Sizer.Add(paramLayout, flag=wx.TOP | wx.RIGHT | wx.LEFT, border=10)
-        # h2Sizer.Add(
-        #     paramFLSizer,
-        #     proportion=1,
-        #     flag=wx.TOP | wx.RIGHT | wx.BOTTOM | wx.EXPAND,
-        #     border=10,
-        # )
-
-        self.SetSizer(hSizer)
+        self.SetSizer(mainSizer)
